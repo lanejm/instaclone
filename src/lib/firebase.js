25b90a4848fafa90bfa92409c2050d59c795eac8
lanejm <67156874+lanejm@users.noreply.github.com>
2021-03-16 13:51:37 -0400

    firebase update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 import { seedDatabase } from "../seed";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 const firebase = window.firebase.initializeApp(firebaseConfig);
